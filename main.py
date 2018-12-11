3874451e5b3fad1a83d8d8f1b218bda1c8309467
CyberDentist <richard@richardbew.com>
2018-12-11 15:47:16 -0600

    Redis login

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 from flask import Flask
+import redis
 app = Flask(__name__)
 
-print("[!] Starting server.")
+redis_host = '127.0.0.1'
+redis_port = 6379
+
+print("[+] Starting portal.")
+
+# REDIS Connection
+db = redis.Redis(host=redis_host, port=redis_port)
+print("[i] Connecting to redis server at " + redis_host + ":" + str(redis_port))
+try:
+	print("[+] Connected: Redis v" + db.info(section='server')['redis_version']);
+except:
+	print("[-] Could not establish connection to redis.")
+
 
 @app.route("/")
 def hello():
