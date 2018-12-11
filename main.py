3f6236a1eda938360659bcc8b0a90ec3cb554105
CyberDentist <richard@richardbew.com>
2018-12-11 16:17:25 -0600

    Update

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 from flask import Flask
-import redis
+import redis, sys
 app = Flask(__name__)
 
+# PARAMETERS
 redis_host = '127.0.0.1'
 redis_port = 6379
 
@@ -10,16 +11,21 @@ print("[+] Starting portal.")
 
 # REDIS Connection
 db = redis.Redis(host=redis_host, port=redis_port)
-print("[i] Connecting to redis server at " + redis_host + ":" + str(redis_port))
+print("[i] Connecting to redis server at " + redis_host + ":" + str(redis_port) + ".")
 try:
 	print("[+] Connected: Redis v" + db.info(section='server')['redis_version']);
 except:
-	print("[-] Could not establish connection to redis.")
+	print("[-] Could not establish connection to redis. Check config.")
+	print("[-] Stopping.")
+	sys.exit()
+
 
 
 @app.route("/")
-def hello():
-    return "Awww... Puppy!"
+def root():
+    return "OK"
 
+# EXECUTABLE 
+# (Unnecessary if this file is not the entrypoint)
 if __name__ == '__main__':
     app.run()
