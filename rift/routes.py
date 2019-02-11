e797f3aaa5e31183979294b0d2849c1799311b75
Richard Bew <Richard.P.Bew@uscga.edu>
2019-02-11 15:47:44 -0500

    Authentication (in progress)

--- rift/routes.py
+++ rift/routes.py
@@ -1,4 +1,4 @@
-﻿from flask import Blueprint, render_template
+﻿from flask import Blueprint, render_template, request
 from mongoengine import Document
 import rift.nav as nav
 import rift.models as models
@@ -10,7 +10,10 @@ def root():
 	menu = nav.items
 	return render_template('home.html', menu=menu, Motd=models.Motd)
 
-@main.route("/login")
+@main.route("/login", methods=['GET','POST'])
 def login():
-	menu = nav.items
-	return render_template('login.html', menu=menu)
+	if request.method == 'POST':
+		return "LOGIN! " + request.authorization['username']
+	else:
+		menu = nav.items
+		return render_template('login.html', menu=menu)
