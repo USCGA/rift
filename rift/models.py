d1a60de4cc3f44924c7becf312943d4b13879923
Richard Bew <11297477+CyberDentist@users.noreply.github.com>
2019-02-11 21:56:11 -0500

    Login/Registration

--- rift/models.py
+++ rift/models.py
@@ -2,10 +2,15 @@
 import datetime
 from mongoengine import Document
 from mongoengine.fields import \
-	(DateTimeField, EmbeddedDocumentField, ListField, ReferenceField, StringField)
+	(DateTimeField, EmbeddedDocumentField, ListField, ReferenceField, StringField, EmailField)
 
 # Models
 class Motd(Document):
 	title = StringField(max_length=200, required=True)
 	message = StringField(max_length=500, required=True)
 	date_created = DateTimeField(default=datetime.datetime.utcnow)
+
+class User(Document):
+	username = StringField(max_length=32, required=True, unique=True)
+	email = EmailField(max_length=64, unique=True)
+	password = StringField(required=True)
--- rift/routes.py
+++ rift/routes.py
@@ -1,5 +1,6 @@
 ﻿from flask import Blueprint, render_template, request
-from mongoengine import Document
+from mongoengine import Document, DoesNotExist, MultipleObjectsReturned
+from passlib.hash import pbkdf2_sha256
 import rift.nav as nav
 import rift.models as models
 
@@ -12,8 +13,41 @@ def root():
 
 @main.route("/login", methods=['GET','POST'])
 def login():
+	# TODO Add validation to email/uname/pword.
 	if request.method == 'POST':
-		return "LOGIN! " + request.form['uname']
+		if request.form['type'] == "login": # ----------- LOGIN
+			uname = request.form['uname']
+			pword = request.form['pword']
+			# Retrieve user from db.
+			try:
+				user = models.User.objects.get(username=uname)
+			except DoesNotExist:
+				return uname + " DNE."
+			except MultipleObjectsReturned:
+				return "Somehow, there is more than \
+					one user by the name of " + uname
+			# Ensure password match.
+			if pbkdf2_sha256.verify(pword, user.password):
+				# Good Login.
+				# TODO issue login token.
+				return "GOOD LOGIN, brother."
+			else:
+				# Bad Login.
+				# TODO wrong password alert. Will probably involve html/js.
+				return "BAD LOGIN, doodsicle"
+		elif request.form['type'] == "register": # ------ REGISTER
+			# Hash password.
+			pword_hash = pbkdf2_sha256.hash(request.form['pword'])
+			# Create New User.
+			new_user = models.User()
+			new_user.email = request.form['email']
+			new_user.username = request.form['uname']
+			new_user.password = pword_hash
+			new_user.save()
+			# TODO Login after successful.
+		else:
+			return "BAD POST, bro."
+		return "uname: " + request.form['uname'] + " registered."
 	else:
 		menu = nav.items
 		return render_template('login.html', menu=menu)
